182ea789fc135e696291e7fd77a6f49d4d7d29c6
Jason Francis <jason@halfspiral.com>
2015-09-21 15:15:07 -0700

    Remove browser code

--- browser/webpack.config.dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  context: "./app/scripts",
-  entry: "./main",
-  output: {
-      path: "./app/scripts/bundle",
-      filename: "bundle.js"
-  }
-};
--- browser/webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  context: "./app/scripts",
-  entry: "./main",
-  output: {
-      path: "./dist/scripts/bundle",
-      filename: "bundle.js"
-  }
-};
