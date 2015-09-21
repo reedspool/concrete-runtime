f5b31d51c0bbdda2869cc790808c5f898697b4a7
Jason Francis <jason@halfspiral.com>
2015-09-21 16:09:35 -0700

    Library-ify

---
+++ concrete.js
@@ -0,0 +1,10 @@
+module.exports = {
+  BaconUniverse: require('./lib/BaconUniverse'),
+  Block: require('./lib/Block'),
+  ConcreteParser: require('./lib/ConcreteParser'),
+  Tape: require('./lib/Tape'),
+  Universe: require('./lib/Universe'),
+
+  config: require('./lib/config'),
+  util: require('./lib/util')
+};
